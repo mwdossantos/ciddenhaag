74d5a340e5d4330ad71d0fdda07f0435153a7fd8
Glitch (ciddenhaag) <none>
2019-05-31 05:15:40 +0000

    🏓🌽 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,108 @@
+(function() {
+
+   // header
+   ScrollReveal().reveal('.nav-bar', {
+     delay: 300,
+     distance: '50px',
+     origin: 'right',
+   });
+   ScrollReveal().reveal('.header-logo', {
+     delay: 300,
+     distance: '50px',
+     origin: 'bottom',
+   });
+
+   // section 1
+   ScrollReveal().reveal('.section-1-bg', {
+     delay: 300,
+   });
+   ScrollReveal().reveal('.video-container', {
+     delay: 300,
+     distance: '200px',
+     origin: 'right',
+   });
+   ScrollReveal().reveal('.section-1-logo', {
+     delay: 300,
+     distance: '100px',
+     origin: 'left',
+   });
+
+   // section 2
+   ScrollReveal().reveal('.section-2-blob', {
+     delay: 300,
+     distance: '50px',
+     origin: 'bottom',
+   });
+   ScrollReveal().reveal('.section-2-promo', {
+     delay: 600,
+     distance: '20px',
+     origin: 'bottom',
+   });
+   ScrollReveal().reveal('.text-block', {
+     delay: 300,
+     distance: '20px',
+     origin: 'left',
+   });
+
+   // section 3
+   ScrollReveal().reveal('.section-3-blob', {
+     delay: 300,
+     distance: '50px',
+     origin: 'bottom',
+   });
+   ScrollReveal().reveal('.section-3-promo', {
+     delay: 600,
+     distance: '20px',
+     origin: 'bottom',
+   });
+   ScrollReveal().reveal('.map-container', {
+     delay: 300,
+     distance: '20px',
+     origin: 'right',
+   });
+
+   // section 5
+   ScrollReveal().reveal('.section-5-blob', {
+     delay: 300,
+     distance: '50px',
+     origin: 'bottom',
+   });
+   ScrollReveal().reveal('.section-5-promo', {
+     delay: 400,
+     distance: '20px',
+     origin: 'bottom',
+   });
+
+   // footer
+   ScrollReveal().reveal('.footer-container', {
+     delay: 300,
+     distance: '50px',
+     origin: 'bottom',
+   });
+
+   ScrollReveal().reveal('.f-menu', {
+     delay: 400,
+     distance: '20px',
+     origin: 'bottom',
+   });
+
+   ScrollReveal().reveal('.deelgebieden', {
+     delay: 500,
+     distance: '20px',
+     origin: 'bottom',
+   });
+
+   ScrollReveal().reveal('.nieuwsbrief', {
+     delay: 600,
+     distance: '20px',
+     origin: 'bottom',
+   });
+
+   ScrollReveal().reveal('.icons, .copyright-text', {
+     delay: 300,
+     distance: '20px',
+     origin: 'bottom',
+   });
+
+
+})();
